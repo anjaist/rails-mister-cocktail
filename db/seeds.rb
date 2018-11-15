d830b36bc8e8903cdfae6105e0a84a6be4c3e78f
Anja Istenic <aistenic1@gmail.com>
2018-11-15 14:23:34 +0100

    added MVC for Cocktail and M for Dose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "pineapple")
+Ingredient.create(name: "orange zest")
+Ingredient.create(name: "rosemary leaves")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "shaved ice")
+Ingredient.create(name: "cherry")
+Ingredient.create(name: "olive")
+Ingredient.create(name: "onion")
+Ingredient.create(name: "thyme leaves")
